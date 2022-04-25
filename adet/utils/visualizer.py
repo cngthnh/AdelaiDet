7708b27e6aa7c54c8597b2ffde9a336d944c2e08
Cong Thanh Vu <zarelasez@gmail.com>
2022-04-25 17:15:11 +0700

    Added fonts and fixed visualizer

--- adet/utils/visualizer.py
+++ adet/utils/visualizer.py
@@ -4,13 +4,174 @@ from detectron2.utils.visualizer import Visualizer
 import matplotlib.colors as mplc
 import matplotlib.font_manager as mfm
 
+dictionary = "aàáạảãâầấậẩẫăằắặẳẵAÀÁẠẢÃĂẰẮẶẲẴÂẦẤẬẨẪeèéẹẻẽêềếệểễEÈÉẸẺẼÊỀẾỆỂỄoòóọỏõôồốộổỗơờớợởỡOÒÓỌỎÕÔỒỐỘỔỖƠỜỚỢỞỠiìíịỉĩIÌÍỊỈĨuùúụủũưừứựửữƯỪỨỰỬỮUÙÚỤỦŨyỳýỵỷỹYỲÝỴỶỸ"
+
+
+def make_groups():
+    groups = []
+    i = 0
+    while i < len(dictionary) - 5:
+        group = [c for c in dictionary[i : i + 6]]
+        i += 6
+        groups.append(group)
+    return groups
+
+
+groups = make_groups()
+
+TONES = ["", "ˋ", "ˊ", "﹒", "ˀ", "˜"]
+SOURCES = ["ă", "â", "Ă", "Â", "ê", "Ê", "ô", "ơ", "Ô", "Ơ", "ư", "Ư", "Đ", "đ"]
+TARGETS = ["aˇ", "aˆ", "Aˇ", "Aˆ", "eˆ", "Eˆ", "oˆ", "o˒", "Oˆ", "O˒", "u˒", "U˒", "D-", "d-"]
+
+
+def correct_tone_position(word):
+    word = word[:-1]
+    if len(word) < 2:
+        pass
+    first_ord_char = ""
+    second_order_char = ""
+    for char in word:
+        for group in groups:
+            if char in group:
+                second_order_char = first_ord_char
+                first_ord_char = group[0]
+    if word[-1] == first_ord_char and second_order_char != "":
+        pair_chars = ["qu", "Qu", "qU", "QU", "gi", "Gi", "gI", "GI"]
+        for pair in pair_chars:
+            if pair in word and second_order_char in ["u", "U", "i", "I"]:
+                return first_ord_char
+        return second_order_char
+    return first_ord_char
+
+
+def decoder(recognition):
+    for char in TARGETS:
+        recognition = recognition.replace(char, SOURCES[TARGETS.index(char)])
+    if len(recognition) < 1:
+        return recognition
+    if recognition[-1] in TONES:
+        if len(recognition) < 2:
+            return recognition
+        replace_char = correct_tone_position(recognition)
+        tone = recognition[-1]
+        recognition = recognition[:-1]
+        for group in groups:
+            if replace_char in group:
+                recognition = recognition.replace(replace_char, group[TONES.index(tone)])
+    return recognition
+
 class TextVisualizer(Visualizer):
     def __init__(self, image, metadata, instance_mode, cfg):
         Visualizer.__init__(self, image, metadata, instance_mode=instance_mode)
         self.voc_size = cfg.MODEL.BATEXT.VOC_SIZE
         self.use_customer_dictionary = cfg.MODEL.BATEXT.CUSTOM_DICT
         if not self.use_customer_dictionary:
-            self.CTLABELS = [' ','!','"','#','$','%','&','\'','(',')','*','+',',','-','.','/','0','1','2','3','4','5','6','7','8','9',':',';','<','=','>','?','@','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','[','\\',']','^','_','`','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','{','|','}','~']
+            self.CTLABELS = [
+            " ",
+            "!",
+            '"',
+            "#",
+            "$",
+            "%",
+            "&",
+            "'",
+            "(",
+            ")",
+            "*",
+            "+",
+            ",",
+            "-",
+            ".",
+            "/",
+            "0",
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            ":",
+            ";",
+            "<",
+            "=",
+            ">",
+            "?",
+            "@",
+            "A",
+            "B",
+            "C",
+            "D",
+            "E",
+            "F",
+            "G",
+            "H",
+            "I",
+            "J",
+            "K",
+            "L",
+            "M",
+            "N",
+            "O",
+            "P",
+            "Q",
+            "R",
+            "S",
+            "T",
+            "U",
+            "V",
+            "W",
+            "X",
+            "Y",
+            "Z",
+            "[",
+            "\\",
+            "]",
+            "^",
+            "_",
+            "`",
+            "a",
+            "b",
+            "c",
+            "d",
+            "e",
+            "f",
+            "g",
+            "h",
+            "i",
+            "j",
+            "k",
+            "l",
+            "m",
+            "n",
+            "o",
+            "p",
+            "q",
+            "r",
+            "s",
+            "t",
+            "u",
+            "v",
+            "w",
+            "x",
+            "y",
+            "z",
+            "{",
+            "|",
+            "}",
+            "~",
+            "ˋ",
+            "ˊ",
+            "﹒",
+            "ˀ",
+            "˜",
+            "ˇ",
+            "ˆ",
+            "˒",
+            "‑",
+        ]
         else:
             with open(self.use_customer_dictionary, 'rb') as fp:
                 self.CTLABELS = pickle.load(fp)
@@ -42,13 +203,10 @@ class TextVisualizer(Visualizer):
         for c in rec:
             c = int(c)
             if c < self.voc_size - 1:
-                if self.voc_size == 96:
-                    s += self.CTLABELS[c]
-                else:
-                    s += str(chr(self.CTLABELS[c]))
+                s += self.CTLABELS[c]
             elif c == self.voc_size -1:
                 s += u'口'
-        return s
+        return decoder(s)
 
     def _ctc_decode_recognition(self, rec):
         # ctc decoding
@@ -58,12 +216,8 @@ class TextVisualizer(Visualizer):
             c = int(c)
             if c < self.voc_size - 1:
                 if last_char != c:
-                    if self.voc_size == 96:
-                        s += self.CTLABELS[c]
-                        last_char = c
-                    else:
-                        s += str(chr(self.CTLABELS[c]))
-                        last_char = c
+                    s += self.CTLABELS[c]
+                    last_char = c
             elif c == self.voc_size -1:
                 s += u'口'
             else:
@@ -91,7 +245,7 @@ class TextVisualizer(Visualizer):
                 color=lighter_color,
                 horizontal_alignment=horiz_align,
                 font_size=font_size,
-                draw_chinese=False if self.voc_size == 96 else True
+                draw_chinese=True if self.voc_size > 105 else False
             )
     
 
@@ -128,7 +282,7 @@ class TextVisualizer(Visualizer):
         
         x, y = position
         if draw_chinese:
-            font_path = "./simsun.ttc"
+            font_path = "./fonts/simsun.ttc"
             prop = mfm.FontProperties(fname=font_path)
             self.output.ax.text(
                 x,
@@ -145,6 +299,8 @@ class TextVisualizer(Visualizer):
                 fontproperties=prop
             )
         else:
+            font_path = "./fonts/opensans.ttf"
+            prop = mfm.FontProperties(fname=font_path)
             self.output.ax.text(
                 x,
                 y,
@@ -157,5 +313,6 @@ class TextVisualizer(Visualizer):
                 color=color,
                 zorder=10,
                 rotation=rotation,
+                fontproperties=prop
             )
         return self.output
